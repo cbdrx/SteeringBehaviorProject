aa3cb0c436cb3fa1d5fcae8a7c7b61a0417ad19f
Chris Boudreaux <boudreaux.christopher@gmail.com>
2015-09-16 18:23:38 -0500

    Made changes and added .gitignore

---
+++ Assets/Scripts/FleeBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FleeBehavior : MonoBehaviour {
+
+    public Transform target;
+    public float FleeStrength;
+
+    private Vector3 moveTo;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        moveTo = transform.position - target.position;
+        transform.position += moveTo.normalized * FleeStrength * Time.deltaTime;
+	}
+}
