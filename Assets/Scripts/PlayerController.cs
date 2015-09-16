d1b5c870c81921711ac9c428ca3e12581fcf28d6
Chris Boudreaux <boudreaux.christopher@gmail.com>
2015-09-16 14:47:59 -0500

    Adding the files for the flee behavior

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float horizontal, vertical;
+        horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+
+        transform.position += horizontal * Vector3.right + vertical * Vector3.forward;
+	}
+}
