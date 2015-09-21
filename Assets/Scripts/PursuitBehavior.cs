ac22fdd1c3e04ffe1ba2c88e67d45cc4b2be4853
Chris Boudreaux <boudreaux.christopher@gmail.com>
2015-09-21 14:24:47 -0500

    Adds pursuit behavior

---
+++ Assets/Scripts/PursuitBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PursuitBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float pursuitSpeed;
+    [SerializeField]
+    private float leadOff;
+    [SerializeField]
+    private float criticalDistance;
+
+    private Vector3 targetLastPosition;
+    private Vector3 targetMovement;
+    private Vector3 targetHeading;
+    private Vector3 interceptTarget;
+    private Vector3 movement;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        targetLastPosition = target.position;
+	}
+	
+
+	void Update () 
+    {  
+        //if 
+        if (Vector3.Distance(target.position, transform.position) < criticalDistance)
+        {
+            //if the target is close enough, we want to move directly towards the target
+            movement = (target.position - transform.position).normalized * pursuitSpeed * Time.deltaTime;
+            Debug.Log("Going at him!");
+        }
+        else
+        {
+            //otherwise, we need to lead it off, minimizing the distance travelled
+            targetMovement = target.position - targetLastPosition;
+            targetHeading = target.position + targetMovement;
+            interceptTarget = targetHeading + targetMovement * leadOff;
+            movement = (interceptTarget - transform.position).normalized * pursuitSpeed * Time.deltaTime;      
+        }
+
+        transform.position += movement;
+        targetLastPosition = target.position;
+        
+        Debug.DrawLine(transform.position, transform.position + movement.normalized * 10, Color.red);
+	}
+}
---
+++ Assets/Scripts/SeekBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeekBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    
+    [SerializeField]
+    private float seekSpeed;
+
+    private Vector3 moveTo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveTo = target.position - transform.position;
+        transform.position += seekSpeed * moveTo.normalized * Time.deltaTime;
+	
+	}
+}
