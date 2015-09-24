a51a65ba4e7b9e3102ec2d10046aed219ef93661
Chris Boudreaux <boudreaux.christopher@gmail.com>
2015-09-24 01:05:48 -0500

    Adds multi-flock functionality

---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlockManager : MonoBehaviour {
+
+    public Transform target;
+    public GameObject flockObject;
+    public int numberOfFlocks, numberOfBoids;
+    public bool chase;
+    private Random r = new Random();
+
+    private GameObject flockTemp;
+    private int boidsLeft;
+    private int BoidsInFlock;
+    private GameObject[] flocks;
+    private bool chaseLast;
+	// Use this for initialization
+	void Start () 
+    {
+        flocks = new GameObject[numberOfFlocks];
+
+        boidsLeft = numberOfBoids;
+        for(int i = 0; i < numberOfFlocks-1; i++)
+        {
+            flockTemp = Instantiate(flockObject);
+            BoidsInFlock = (int) Random.Range(0,(boidsLeft / 2));
+            flockTemp.GetComponent<Flock_Boids_Manager>().initialNumberOfBoids = BoidsInFlock;
+            flockTemp.GetComponent<Flock_Boids_Manager>().target = target;
+            boidsLeft -= BoidsInFlock;
+            flocks[i] = flockTemp;
+        }
+        flockTemp = Instantiate(flockObject);
+        flockTemp.GetComponent<Flock_Boids_Manager>().initialNumberOfBoids = boidsLeft;
+        flockTemp.GetComponent<Flock_Boids_Manager>().target = target;
+        flocks[numberOfFlocks - 1] = flockTemp;
+
+	}
+
+    // Update is called once per frame
+    void Update() 
+    {
+        if (chase != chaseLast)
+        {
+            for (int i = 0; i < numberOfFlocks; i++)
+            {
+                flocks[i].GetComponent<Flock_Boids_Manager>().chase = chase;
+            }
+            chaseLast = chase;
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerControllerFlocks.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerFlocks : MonoBehaviour
+{
+
+    public float speed;
+    public FlockManager manager;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal, vertical;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space)) manager.chase = !(manager.chase);
+
+        //if (Input.GetKeyDown(KeyCode.KeypadMinus) && manager.dodgeRadius > 10) manager.dodgeRadius -= 5;
+        //if (Input.GetKeyDown(KeyCode.KeypadPlus) && manager.dodgeRadius < 50) manager.dodgeRadius += 5;
+
+        transform.position +=
+            (horizontal * Vector3.right + vertical * Vector3.forward).normalized * speed * Time.deltaTime;
+
+    }
+}
